aabb0a98e8803c7fab4194e8c198026b16b22ca6
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-06-14 06:05:58 +0530

    Add files via upload

---
+++ Abc1/Abc1/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub PictureBox1_Click(sender As System.Object, e As System.EventArgs) Handles PictureBox1.Click
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs)
+
+
+
+    End Sub
+
+    Private Sub get_iamge(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
